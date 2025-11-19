9bb3b6a50663e920ef7bd3a5a7e401c00910254a
masuddev2 <tuhin.semicolonit@gmail.com>
2025-11-19 10:55:40 +0600

    api route

--- server/class_express/server.js
+++ server/class_express/server.js
@@ -2,7 +2,37 @@ const express = require("express");
 const app = express();
 
 const port = 3900;
+app.use(express.json());
+// app.get("/users", (req, res) => {
+//   res.send("users Server is running...");
+// });
+
+// app.get("/user", (req, res) => {
+//   res.send("user Server is running...");
+// });
+
+// app.get("/home", (req, res) => {
+//   res.send("Home Server is running...");
+// });
+
+app.get("/create", (req, res) => {
+  res.send("Get Api created.");
+});
+
+app.post("/create", (req, res) => {
+  console.log("req", req.body);
+  res.send(req.body);
+});
+
+app.put("/create", (req, res) => {
+  console.log("req", req.body);
+  res.send("Put Api created.");
+});
+
+app.delete("/create", (req, res) => {
+  res.send("Delete Api created.");
+});
 
 app.listen(port, () => {
-  console.log(`Express server running.... http://loclhost:${port}`);
+  console.log(`Express server running.... http://localhost:${port}`);
 });
