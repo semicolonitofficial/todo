b20188f399df3f9865ae3d34fcd7052d3dfa9f9d
masuddev2 <tuhin.semicolonit@gmail.com>
2025-11-26 10:40:08 +0600

    express server with mongodb connedtion successfull

--- server/class_express/server.js
+++ server/class_express/server.js
@@ -1,18 +1,28 @@
 const express = require("express");
-const mongoose = require("mongoose");
+
 const User = require("./models/user");
 const app = express();
 
 const port = 3900;
 app.use(express.json());
 
+// mongodb+srv://gowtamsemicolonit_db_user:ihq1D1rqMUsH7PRF@cluster0.0mhcufk.mongodb.net/?appName=Cluster0
+const mongoose = require("mongoose");
 const mongoDBURL =
-  "mongodb+srv://gowtamsemicolonit_db_user:jyRz0PqH2zBnXiHg@cluster0.vpvtd4n.mongodb.net/?appName=Cluster0";
+  "mongodb+srv://gowtamsemicolonit_db_user:ihq1D1rqMUsH7PRF@cluster0.0mhcufk.mongodb.net/?appName=Cluster0";
 
 mongoose
   .connect(mongoDBURL)
   .then(() => console.log("Database Connection Successful"))
   .catch((err) => console.error("Connection Error:", err));
+
+// const mongoDBURL =
+//   "mongodb+srv://gowtamsemicolonit_db_user:jyRz0PqH2zBnXiHg@cluster0.vpvtd4n.mongodb.net/?appName=Cluster0";
+
+// mongoose
+//   .connect(mongoDBURL)
+//   .then(() => console.log("Database Connection Successful"))
+//   .catch((err) => console.error("Connection Error:", err));
 // app.get("/users", (req, res) => {
 //   res.send("users Server is running...");
 // });
@@ -74,10 +84,10 @@ app.get("/create/:id", (req, res) => {
 app.post("/create", async (req, res) => {
   console.log("req", req.body);
 
-  const dbdata = User(req.body);
+  // const dbdata = User(req.body);
 
-  const resDATa = await dbdata.save();
-  res.status(201).json({ message: "Data insert successfully done", resDATa });
+  // const resDATa = await dbdata.save();
+  res.status(201).json({ message: "Data insert successfully done" });
 });
 
 app.delete("/create/:id", (req, res) => {
